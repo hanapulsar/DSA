e83c232cc8eaa991efbe09a3f2e4d52a5a48016d
hanapulsar <217953127+hanapulsar@users.noreply.github.com>
2026-04-23 05:17:36 +0900

    Add resize, insert, insert_or_assign

--- HashTable.h
+++ HashTable.h
@@ -1,6 +1,8 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
+#include <iostream>
+
 template <typename T>
 class HashTable {
 private:
@@ -24,13 +26,16 @@ private:
 	int calculate_l(int size);
 	int calculate_capacity(int power);
 	int lcg();
+	void resize();
 public:
 	HashTable(int size);
 	HashTable(int size, bool random);
 	HashTable(const HashTable& src);
 	~HashTable();
 	HashTable& operator=(const HashTable& src);
-	void HashTable<T>::print() const;
+	void print() const;
+	bool insert(int key, const T& value);
+	void insert_or_assign(int key, const T& value);
 };
 
 template <typename T>
@@ -64,6 +69,24 @@ int HashTable<T>::lcg() {
 	static size_t x = 0;
 	x = (1021 * x + 24631) % 116640;
 	return static_cast<int>(x);
+}
+
+template <typename T>
+void HashTable<T>::resize() {
+	int old_capacity = table_capacity;
+	Node* old_table = table;
+
+	l++;
+	table_capacity = calculate_capacity(l);
+	table = new Node[table_capacity];
+	table_size = 0;
+
+	for (int i = 0; i < old_capacity; ++i) {
+		if (old_table[i].status == OCCUPIED) {
+			insert(old_table[i].key, old_table[i].value);
+		}
+	}
+	delete[] old_table;
 }
 
 template <typename T>
@@ -139,9 +162,49 @@ template <typename T>
 void HashTable<T>::print() const {
 	for (int i = 0; i < table_capacity; ++i) {
 		if (table[i].status == OCCUPIED) {
-			std::cout << "[" << i << "] Key: " << table[i].key << " Value: " << table[i].value << endl;
+			std::cout << "[" << i << "] Key: " << table[i].key << " Value: " << table[i].value << std::endl;
 		}
 	}
+}
+
+template <typename T>
+bool HashTable<T>::insert(int key, const T& value) {
+	if (table_size * 2 >= table_capacity) {
+		resize();
+	}
+
+	int index = hash_function(key);
+	int start_index = index;
+
+	while (table[index].status == OCCUPIED) {
+		if (table[index].key == key) {
+			return false;
+		}
+		index = (index + 1) % table_capacity;
+		if (index == start_index) return false;
+	}
+
+	table[index].key = key;
+	table[index].value = value;
+	table[index].status = OCCUPIED;
+	table_size++;
+	return true;
+}
+
+template <typename T>
+void HashTable<T>::insert_or_assign(int key, const T& value) {
+	int index = hash_function(key);
+	int start_index = index;
+
+	while (table[index].status != EMPTY) {
+		if (table[index].status == OCCUPIED && table[index].key == key) {
+			table[index].value = value;
+			return;
+		}
+		index = (index + 1) % table_capacity;
+		if (index == start_index) break;
+	}
+	insert(key, value);
 }
 
 #endif
