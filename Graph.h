7c5fdcc38eaf071c2befd9f60e062bf4ff53de3f
hanapulsar <217953127+hanapulsar@users.noreply.github.com>
2026-04-25 23:59:18 +0900

    Add get_vertex_index, has_vertex, add_vertex, remove_vertex, vertices

--- Graph.h
+++ Graph.h
@@ -2,15 +2,10 @@
 #define GRAPH_H
 
 #include <vector>
+#include <algorithm>
 
 template<typename Vertex, typename Distance = double>
 class Graph {
-private:
-	std::vector<Vertex> vertices;
-	std::vector<std::vector<std::vector<Edge>>> matrix;
-
-	int get_vertex_index(const Vetrex& v) const;
-
 public:
 	struct Edge {
 		Vertex from;
@@ -18,6 +13,13 @@ public:
 		Distance distance;
 	};
 
+private:
+	std::vector<Vertex> _vertices;
+	std::vector<std::vector<std::vector<Edge>>> _matrix;
+
+	long long get_vertex_index(const Vertex& v) const;
+
+public:
 	bool has_vertex(const Vertex& v) const;
 	bool add_vertex(const Vertex& v);
 	bool remove_vertex(const Vertex& v);
@@ -25,8 +27,54 @@ public:
 };
 
 template<typename Vertex, typename Distance>
+long long Graph<Vertex, Distance>::get_vertex_index(const Vertex& v) const {
+	auto it = std::find(_vertices.begin(), _vertices.end(), v);
+	if (it != _vertices.end()) {
+		return std::distance(_vertices.begin(), it);
+	}
+	return -1;
+}
+
+template<typename Vertex, typename Distance>
 bool Graph<Vertex, Distance>::has_vertex(const Vertex& v) const {
-	
+	return get_vertex_index(v) != -1;
+}
+
+template<typename Vertex, typename Distance>
+bool Graph<Vertex, Distance>::add_vertex(const Vertex& v) {
+	if (has_vertex(v)) {
+		return false;
+	}
+
+	_vertices.push_back(v);
+
+	for (auto& row : _matrix) {
+		row.push_back(std::vector<Edge>());
+	}
+
+	_matrix.push_back(std::vector<std::vector<Edge>>(_vertices.size()));
+
+	return true;
+}
+
+template<typename Vertex, typename Distance>
+bool Graph<Vertex, Distance>::remove_vertex(const Vertex& v) {
+	long long it = get_vertex_index(v);
+
+	_matrix.erase(_matrix.begin() + it);
+
+	for (auto& row : _matrix) {
+		row.erase(row.begon() + it);
+	}
+
+	_vertices.erase(_vertices.begin + id);
+
+	return true;
+}
+
+template<typename Vertex, typename Distance>
+std::vector<Vertex>  Graph<Vertex, Distance>::vertices() const {
+	return _vertices;
 }
 
 #endif // GRAPH_H
